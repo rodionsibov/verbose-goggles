76b84f51e8035a9066c3da4ad1bf7dc32153425f
Rodion Sibov <rodionsibov@gmail.com>
2021-04-30 01:23:29 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,51 @@
 <template>
-  
-  <router-view />
+  <header>
+    <router-link to="/">
+      <h1><span>Vue</span>Movies</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;700&display=swap");
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira Sans", sans-serif;
+
+  &::selection {
+    background: transparentize($color: #428883, $amount: 0.5);
+  }
+}
+
+body {
+  background-color: #35495e;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2c3d4e;
+  box-shadow: 0 0 6px rgba($color: #000000, $alpha: 0.1);
+
+  h1 {
+    color: #fff;
+    font-size: 28px;
+
+    span {
+      color: #428883;
+    }
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,56 @@
 <template>
   <div class="home">
-    Home
+    <div class="feature-card">
+      <router-link to="/movie/tt0409591">
+        <img src="https://picsum.photos/400/" alt="" class="featured-image" />
+        <div class="detail">
+          <h3>Naruto</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit et,
+            tempore eligendi adipisci animi sequi?
+          </p>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
- 
-}
+export default {};
 </script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+
+    .featured-image {
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+      position: relative;
+      z-index: 0;
+    }
+
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba($color: #000000, $alpha: 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color: #fff;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
